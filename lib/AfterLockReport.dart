c3f35770083f0d3d6d16a161800c548ca10af38d
Rohan Gautam <rohan1gautam@gmail.com>
2019-09-29 09:14:26 +0800

    added pdf report generation

--- lib/AfterLockReport.dart
+++ lib/AfterLockReport.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:waste_management/MainPage.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pdf;
+import 'package:printing/printing.dart';
 
 class AfterLockReport extends StatefulWidget {
   var weight, destination, src, volume;
@@ -21,11 +24,20 @@ class _AfterLockReportState extends State<AfterLockReport> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text("$name", style: TextStyle(fontSize: 15), overflow: TextOverflow.ellipsis,),
+              Text(
+                "$name",
+                style: TextStyle(fontSize: 15),
+                overflow: TextOverflow.ellipsis,
+              ),
               VerticalDivider(
                 thickness: 2,
               ),
-              Text("$info $unit", style: TextStyle(fontSize: 14), overflow: TextOverflow.ellipsis,maxLines: 2,)
+              Text(
+                "$info $unit",
+                style: TextStyle(fontSize: 14),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              )
             ],
           )),
       onPressed: null,
@@ -67,6 +79,47 @@ class _AfterLockReportState extends State<AfterLockReport> {
     })); // change to chat page for testing terminal commands
   }
 
+  void exportPdf() {
+    Printing.layoutPdf(
+      onLayout: buildPdf,
+    );
+  }
+
+  List<int> buildPdf(PdfPageFormat format) {
+    final pdf.Document doc = pdf.Document();
+
+    doc.addPage(
+      pdf.Page(
+        pageFormat: format,
+        build: (pdf.Context context) {
+          return pdf.ConstrainedBox(
+            constraints: const pdf.BoxConstraints.expand(),
+            child: pdf.FittedBox(
+              child: pdf.Column(
+                children: <pdf.Widget>[
+                  pdf.Text('Bin lock report',
+                      style: pdf.TextStyle(fontSize: 20)),
+                  pdf.Text('Weight: ${widget.weight}',
+                      style: pdf.TextStyle(fontSize: 10)),
+                  pdf.Text('Volume: ${widget.volume}',
+                      style: pdf.TextStyle(fontSize: 10)),
+                  pdf.Text('Destination: ${widget.destination}',
+                      style: pdf.TextStyle(fontSize: 10)),
+                  pdf.Text(
+                    'Source: ${widget.src}',
+                    style: pdf.TextStyle(fontSize: 10),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+
+    return doc.save();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,10 +141,10 @@ class _AfterLockReportState extends State<AfterLockReport> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 infoSnippet("Weight", widget.weight, 'kg'),
-                infoSnippet("Destination", widget.destination,''),
-                infoSnippet("Source", widget.src,''),
-                infoSnippet("LatLong", widget.latlng,''),
-                infoSnippet("Volume", widget.volume,'cm3'),
+                infoSnippet("Destination", widget.destination, ''),
+                infoSnippet("Source", widget.src, ''),
+                infoSnippet("LatLong", widget.latlng, ''),
+                infoSnippet("Volume", widget.volume, 'cm3'),
               ],
             ),
             Column(
